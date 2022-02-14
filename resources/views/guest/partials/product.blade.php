ccebc893a58a7f7692e8311ab76e98a5d38b021a
filauro1989 <filauro1989@gmail.com>
2022-02-14 17:15:36 +0100

    talent and spec

--- resources/views/guest/partials/product.blade.php
+++ resources/views/guest/partials/product.blade.php
@@ -40,6 +40,56 @@
         </div>
     </div>
     <div class="product-bottom-container">
+        <div class="container-55 d-flex">
+            <div class="talent-box w-50">
+                <h2>Talent</h2>
+                <div class="talent-box-artby d-flex">
+                    <p class="w-30">Art by:</p>
+                    <p class="w-70 artists">
+                        @foreach ($product['artists'] as $artist)
+                            @if ($loop->last)
+                            <span><a href="">{{ $artist }}</a></span>
+                            @else 
+                            <span><a href="">{{ $artist }}</a>,</span>
+                            @endif
+                        @endforeach
+                    </p>
+                </div>
+                <div class="talent-box-writtenby d-flex">
+                    <p class="w-30">Written by:</p>
+                    <p class="w-70 artists">
+                        @foreach ($product['writers'] as $writer)
+                            @if ($loop->last)
+                            <span><a href="">{{ $writer }}</a></span>
+                            @else 
+                            <span><a href="">{{ $writer }}</a>,</span>
+                            @endif
+                        @endforeach
+                    </p>
+                </div>
+            </div>
+            <div class="specs-box w-50">
+                <h2>Specs</h2>
+                <div class="specs-box-series d-flex">
+                    <p class="w-30">Series:</p>
+                    <p class="w-70">
+                            {{ $product['series'] }}
+                    </p>
+                </div>
+                <div class="specs-box-price d-flex">
+                    <p class="w-30">Price:</p>
+                    <p class="w-70">
+                            {{ $product['price'] }}
+                    </p>
+                </div>
+                <div class="specs-box-sale d-flex">
+                    <p class="w-30">On Sale Date:</p>
+                    <p class="w-70">
+                        {{ date_format(date_create($product['sale_date']),'M d Y') }}
+                    </p>
+                </div>
 
+            </div>
+        </div>
     </div>
 @endsection
